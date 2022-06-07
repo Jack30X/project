df44c40205fc47f05fd7d497db76f38154d348d8
Jack <kozlov.jenia@gmail.com>
2022-06-07 18:18:08 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+"use sctrict"
+
+//alert('Hello');
+
+//const result = confirm("Are you here?");
+//console.log(result);
+
+// const answer = prompt("Вам є 18?", "18");
+// console.log(answer + 5);
+
+const answers = [];
+
+answers[0] = prompt('Як вас імя?', '');
+answers[1] = prompt('Як вас фамілія?', '');
+answers[2] = prompt('Скільки вам років?', '');
+
+document.write(answers);
